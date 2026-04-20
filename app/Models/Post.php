9d175c0918e8daa7fa8b26150a09e2f84ea3b6de
hkndeveloper <hk9916971omg@gmail.com>
2026-04-20 08:58:21 +0300

    fix: update Post model and prepare for migration repair

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = ['title', 'slug', 'content', 'author_id', 'is_published', 'published_at'];
 }
